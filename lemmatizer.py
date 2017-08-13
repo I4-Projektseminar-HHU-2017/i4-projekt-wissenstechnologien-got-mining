c85b93077eb399cbf57b79d6ec22928473ab7866
NatiHHU <nataliesingh1995@gmail.com>
2017-08-13 18:19:09 +0200

    Delete lemmatizer.py

--- lemmatizer.py
+++
@@ -1,8 +0,0 @@
-from nltk.stem import WordNetLemmatizer
-
-wordnet_lemmatizer = WordNetLemmatizer()
-liste = ['kills', 'killing', 'slaughtered', 'beat', 'died']
-
-for word in liste:
-    output = wordnet_lemmatizer.lemmatize(word, pos='v')
-    print(output)
