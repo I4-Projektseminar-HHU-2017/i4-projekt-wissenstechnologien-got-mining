b04bccf1c50b39edd5ba040e292b36b8c8c9371e
NicoHHU <niebu@web.de>
2017-08-13 18:01:33 +0200

    Add files via upload

---
+++ JonKnowsNothing.py
@@ -0,0 +1,18 @@
+import re 
+
+
+
+string_lst = ['you know nothing, Jon Snow']
+string_lst2 = ['You know nothing, Jon Snow']
+
+text = open("1-5buch.txt", "r").read()
+
+pew = re.findall(r"(?=("+'|'.join(string_lst)+r"))",text)
+peww = re.findall(r"(?=("+'|'.join(string_lst2)+r"))",text)
+print(pew)
+print (peww)
+
+
+print (pew.count("you know nothing, Jon Snow"))
+print (peww.count("You know nothing, Jon Snow"))
+
